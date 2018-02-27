32bcb44f4b7603996126b77fc7fe84bbcd46d9e6
inedostoev <inedostoev@gmail.com>
2018-02-27 19:10:36 +0300

    first h/w

--- L1-intro/src/main/java/ru/track/App2.java
+++ L1-intro/src/main/java/ru/track/App2.java
@@ -1,12 +1,26 @@
 package ru.track;
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
 
-import org.apache.commons.lang3.StringUtils;
 
 public class App2 {
 
-    public static void main(String[] args) {
-        String str = StringUtils.capitalize(args[0]);
-        System.out.println(str);
+    public static final String URL = "http://guarded-mesa-31536.herokuapp.com/track";
+    public static final String FIELD_NAME = "name";
+    public static final String FIELD_GITHUB = "github";
+    public static final String FIELD_EMAIL = "email";
+    public static boolean success = false;
+
+    public static void main(String[] args) throws Exception {
+        HttpResponse<JsonNode> Response = Unirest.post(URL)
+                .field (FIELD_NAME,"Kirill")
+                .field (FIELD_GITHUB, "https://github.com/inedostoev")
+                .field (FIELD_EMAIL, "inedostoev@mail.ru")
+                .asJson();
+        success = (Boolean) Response.getBody().getObject().get("success");
+        System.out.println(success);
     }
+
 }
